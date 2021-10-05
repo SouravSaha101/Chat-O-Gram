5b4d6deb507c8249fe12405e21f5a070197644be
Sourav Saha <souravsaha101@gmail.com>
2021-10-05 13:12:57 +0530

    project added

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAWjq4QBGOJ7D9QEx53IPCWhXYI7JpyUX4",
+  authDomain: "chat-a-gram.firebaseapp.com",
+  projectId: "chat-a-gram",
+  storageBucket: "chat-a-gram.appspot.com",
+  messagingSenderId: "576872773154",
+  appId: "1:576872773154:web:e7e746ddb2c97e356e7c7b",
+  measurementId: "G-BE4SEN3FMM",
+};
